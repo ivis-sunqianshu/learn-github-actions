2543c0377ad5e90754f173e89037a2dd8ca9da2a
sunqianshu-ivis <sun.qianshu@ivis.co.jp>
2025-12-15 11:03:01 +0900

    update

---
+++ my-javascript-action/index.js
@@ -0,0 +1,12 @@
+const core = require("@actions/core");
+
+try {
+    const name = core.getInput("name");
+    const message = `Hello, ${name}!`;
+
+    console.log(message);
+
+    core.setOutput("message", message);
+} catch (error) {
+    core.setFailed(error.message);
+}
